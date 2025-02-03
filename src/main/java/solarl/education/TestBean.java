bd0660ac3b8de0e2f33c61eda1745ce647fe17d5
maksim.kovalev <maksim.kovalev@solarl.ru>
2025-02-03 21:47:05 +0300

    Инициализация проекта, добавление самого базового примера

---
+++ src/main/java/solarl/education/TestBean.java
@@ -0,0 +1,11 @@
+package solarl.education;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+
+    public void run() {
+        System.out.println("Bean has been created and running");
+    }
+}
